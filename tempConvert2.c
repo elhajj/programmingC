c5bb6abe42e902bc3f9000c9591d2da7e81d615a
elhajj <tim@telhajj.com>
2015-01-08 18:12:12 -0800

    Exercise 1.4

---
+++ tempConvert2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* print Celsius-Fahrenheit conversion table for celsius = 0, 20, ..., 300 */
+
+int main() {
+    
+
+    float fahr, celsius;   	
+    int lower, upper, step;
+    
+    lower = 0;      	/* lower limit of temprature table */
+    upper = 300;    	/* upper limit of temprature table */
+    step = 20;      	/* step size */
+    
+    celsius = lower;	/* initialize celsius */
+	
+    printf( "Celsius \t Fahrenheit\n"); /*print header */
+
+    while (celsius <= upper) { /* loop test: is celsius less than or equal to the upper range? */ 
+        
+        fahr = (9.0/5.0) * (celsius+32.0); /* if not, then convert celsius to fahr */
+		
+        printf("%3.0f %14.1f\n", celsius, fahr); /* and print result to the screen */
+        
+        celsius = celsius + step; /* than add a step */
+    }
+    
+}
